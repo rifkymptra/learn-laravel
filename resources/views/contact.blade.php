243cb168a9e8cc9dd92e3d58f88195d7cbd18a36
Rifky Maulana Putra <222112322@stis.ac.id>
2024-07-10 22:07:05 +0700

    initial commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact</title>
+</head>
+
+<body>
+    <a href="/">Home</a>
+    <a href="/about"">About</a>
+    <a href="/blog"">Blog</a>
+    <a href="/contact">Contact</a>
+    <h1>Halaman Contact</h1>
+    <ul>
+        <li><a href="https://www.facebook.com">Facebook</a></li>
+        <li><a href="https://www.x.com">X</a></li>
+        <li><a href="https://www.instagram.com">Instagram</a></li>
+    </ul>
+
+</body>
+
+</html>
